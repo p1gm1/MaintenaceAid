5f7ecc5066e3d9d9db6bff483f37310d38ae1cc8
Alberto Perdomo <alperdomo1988@gmail.com>
2021-01-07 16:01:14 -0500

    User model and templates

--- thermoapp/users/urls.py
+++ thermoapp/users/urls.py
@@ -4,9 +4,6 @@ from thermoapp.users.views import (
     user_detail_view,
     user_redirect_view,
     user_update_view,
-    LoginView,
-    SignupView,
-    LogoutView
 )
 
 app_name = "users"
@@ -14,7 +11,4 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
-    path("login/", view=LoginView.as_view(), name="login"),
-    path("signup/", view=SignupView.as_view(), name='signup'),
-    path(route='logout/', view=LogoutView.as_view(), name='logout')
 ]
